618370da5ebf8cbda3b631be83a6c17fa61b4cb6
Teco Wing <jjlucenaguerrero@gmail.com>
2025-04-03 10:32:53 +0200

    Komorebi Windows key shortcuts

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,7 @@
 $Env:XDG_CONFIG_HOME = $Env:APPDATA
 
 $Env:KOMOREBI_CONFIG_HOME = $Env:XDG_CONFIG_HOME
+$Env:WHKD_CONFIG_HOME= $Env:XDG_CONFIG_HOME
 
 $Env:EDITOR = "$env:LOCALAPPDATA\JetBrains\Toolbox\scripts\idea.cmd"
 
