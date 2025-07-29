e09b2ca5372022ecc8de7a20d7bd5fb3fe2e8ad0
JJLucena <jjlucenaguerrero@gmail.com>
2025-07-29 12:58:05 +0200

    Rice terminal with Catppuccin Macchiato

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,4 +5,9 @@ $Env:WHKD_CONFIG_HOME= $Env:XDG_CONFIG_HOME
 
 $Env:EDITOR = "$env:LOCALAPPDATA\JetBrains\Toolbox\scripts\idea.cmd"
 
+Import-Module -Name Terminal-Icons
+
 Set-Alias vim nvim
+
+oh-my-posh init pwsh --config "$HOME\AppData\Roaming\main.omp.json" | Invoke-Expression
+
