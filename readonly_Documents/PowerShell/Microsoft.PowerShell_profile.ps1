e5e3e8d706e87b56ff8bb87a5517b912f27fee46
JJLucena <jjlucenaguerrero@gmail.com>
2025-09-08 10:03:37 +0200

    Add Documents to ignored files

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,6 +5,18 @@ $Env:WHKD_CONFIG_HOME= $Env:XDG_CONFIG_HOME
 
 $Env:EDITOR = "$env:LOCALAPPDATA\JetBrains\Toolbox\scripts\idea.cmd"
 
+# Abilitate tab duplication on windows terminal
+function prompt {
+  $loc = $executionContext.SessionState.Path.CurrentLocation;
+
+$out = ""
+  if ($loc.Provider.Name -eq "FileSystem") {
+    $out += "$([char]27)]9;9;`"$($loc.ProviderPath)`"$([char]27)\"
+  }
+  $out += "PS $loc$('>' * ($nestedPromptLevel + 1)) ";
+  return $out
+}
+
 Import-Module -Name Terminal-Icons
 
 Set-Alias vim nvim
